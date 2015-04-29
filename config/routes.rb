68461231368e7532afc737b89fa29c460c544cd5
Thomas Floch <thomas.floch@gmail.com>
2015-04-29 11:37:02 +1000

    routes set option 'as' to blank to avoid conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   constraints Hangar::RouteConstraint.new do
     FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
-      resources factory, only: [:new, :create], controller: 'hangar/resources'
+      resources factory, only: [:new, :create], controller: 'hangar/resources', as: ''
     end
     delete '/', to: 'hangar/records#delete'
   end
